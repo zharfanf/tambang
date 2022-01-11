3e1d790c80d860e806f172b25fbd233dfeba868b
Faishal Zharfan <zharfanf@gmail.com>
2022-01-11 23:11:03 +0700

    merging sim module to main program

--- sim_modul_arduino/sim_modul_arduino.ino
+++ sim_modul_arduino/sim_modul_arduino.ino
@@ -14,7 +14,8 @@ static double kekeruhan, volta;
 #include <SoftwareSerial.h>
 
 //Create software serial object to communicate with SIM800L
-SoftwareSerial mySerial(3, 2); //SIM800L Tx & Rx is connected to Arduino #3 & #2
+SoftwareSerial simSerial(3, 2); //SIM800L Tx & Rx is connected to Arduino #3 & #2
+SoftwareSerial espSerial(1,0);
 
 void setup()
 {
@@ -22,8 +23,9 @@ void setup()
   Serial.begin(115200);
   
   //Begin serial communication with Arduino and SIM800L
-  mySerial.begin(115200);
-
+  simSerial.begin(115200);
+  espSerial.begin(115200);
+  
   Serial.println("Initializing..."); 
   delay(1000);
 }
@@ -33,25 +35,25 @@ void updateSerial()
   delay(500);
   while (Serial.available()) 
   {
-    mySerial.write(Serial.read());//Forward what Serial received to Software Serial Port
+    simSerial.write(Serial.read());//Forward what Serial received to Software Serial Port
   }
-  while(mySerial.available()) 
+  while(simSerial.available()) 
   {
-    Serial.write(mySerial.read());//Forward what Software Serial received to Serial Port
+    Serial.write(simSerial.read());//Forward what Software Serial received to Serial Port
   }
 }
 
 void sendMessage(){
-  mySerial.println("AT"); //Once the handshake test is successful, it will back to OK
+  simSerial.println("AT"); //Once the handshake test is successful, it will back to OK
   updateSerial();
 
-  mySerial.println("AT+CMGF=1"); // Configuring TEXT mode
+  simSerial.println("AT+CMGF=1"); // Configuring TEXT mode
   updateSerial();
-  mySerial.println("AT+CMGS=\"+6282118988435\"");//change ZZ with country code and xxxxxxxxxxx with phone number to sms
+  simSerial.println("AT+CMGS=\"+6282118988435\"");//change ZZ with country code and xxxxxxxxxxx with phone number to sms
   updateSerial();
-  mySerial.print("Tangki sudah keruh, silahkan dibersihkan"); //text content
+  simSerial.print("Tangki sudah keruh, silahkan dibersihkan"); //text content
   updateSerial();
-  mySerial.write(26);
+  simSerial.write(26);
 
   }
 
@@ -60,12 +62,13 @@ void loop() {
     double ph = ph_calc(), turb = turbidity_calc();
 
     if((turb > 5 || (ph < 7 || ph > 8)) && (stopTime-start > 300000 || stopTime-start == 0)){ // 5 minutes interval
-      //sendMessage();
+      sendMessage();
       start = millis();
     }
-    String data = String(ph);
+    String data = String("|") + String(ph) + String("|") + String(turb) + String("|");
     Serial.print(data);
     Serial.println(turb);
+    espSerial.println(data); // Sending data to esp via serial communication (rx tx pin)
     delay(500);
     stopTime = millis();
 }
